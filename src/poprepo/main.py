f0fb903e062e817519bfc46d6a9e0377ce283669
Sergey.Tolokonnikov <sergey.tolokonnikov@netprotect.com>
2022-04-09 19:29:25 +0300

    tiny fixes

--- src/poprepo/main.py
+++ src/poprepo/main.py
@@ -68,7 +68,7 @@ async def add_process_time_header(request: Request, call_next):
 @app.get("/ping")
 async def ping():
     """
-    Ping/pong for a quick check
+    Ping/pong for a quick health check
     """
     return {"pong": True}
 
